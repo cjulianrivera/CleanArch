7d138fbddb247480b2fa8722908166f86f12d12f
Julian Rivera <rive33774@siigo.com>
2020-03-18 23:22:52 -0500

    commit clear dependency

--- CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infa.Data.Context;
 using CleanArch.Infa.Data.Repository;
+using CleanArch.Infra.Bus;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,11 +19,18 @@ namespace CleanArch.Infra.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
+            //Domain InMemoryBus MediatR
+            services.AddScoped<IMediatorHandler, InMemotyBus>();
+
+            //Domain Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             //Appication Layer
             services.AddScoped<ICourseService, CourseService>();
 
             //Infra.Data Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<UniversityDBContext>();
 
         }
     }
